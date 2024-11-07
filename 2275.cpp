d445a66388dfdb69a5fcde81036c4f8d9ba9d65a
Aryan Bhagat <aryanbhagat5702@gmail.com>
2024-11-07 11:37:47 -0800

    Largest Combination With Bitwise AND Greater Than Zero

--- 2275.cpp
+++ 2275.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int largestCombination(vector<int>& candidates) {
-        int maxNum = *max_element(candidates.begin(), candidates.end());
-        int ans = 0;
-        // maxNum = maxNum >> 1;
-        // cout << maxNum;
-        while (maxNum != 0) {
-
+        int maxNum = INT_MIN;
+        int ans = 0, n = *max_element(candidates.begin(), candidates.end());
+        while (n) {
             int count = 0;
             for (int i = 0; i < candidates.size(); i++) {
-                if (candidates.at(i) & 1) {
+                if ((candidates.at(i) & 1)) {
                     count++;
-                    candidates.at(i) = candidates.at(i) << 1;
                 }
+                candidates.at(i) = candidates.at(i) >> 1;
             }
-            cout << count << endl;
-            if (ans < count)
-                ans = count;
-            maxNum = maxNum << 1;
+            ans = max(ans, count);
+            n = n >> 1;
         }
-
-        // cout << maxNum;
         return ans;
     }
 };
