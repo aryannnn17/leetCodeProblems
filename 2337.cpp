28fe25592f0b71f3469d5b54513f2789616b6806
Aryan Bhagat <aryanbhagat5702@gmail.com>
2024-12-05 11:23:32 -0800

    Move Pieces to Obtain a String

---
+++ 2337.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    void skipSpaces(string& s, int& n, int& pos) {
+        while (pos < n and s[pos] == '_')
+            pos++;
+    }
+public:
+    bool canChange(string start, string target) {
+        int n = start.size();
+        int first = 0, second = 0;
+        int limit_idx = -1;
+        while (first < n) {
+            skipSpaces(start, n, first);
+            skipSpaces(target, n, second);
+            if (first == n and second == n)
+                return true;
+            if (start[first] != target[second])
+                return false;
+
+            if (start[first] == 'L' and (second <= limit_idx or second > first))
+                return false;
+            else if (start[first] == 'R' and first > second)
+                return false;
+            limit_idx = second;
+            first++;
+            second++;
+        }
+        skipSpaces(start, n, first);
+        skipSpaces(target, n, second);
+        return first == n && second == n;
+    }
+};
